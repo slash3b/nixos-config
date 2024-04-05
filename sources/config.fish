efc05212368778860321ccb349d9c8bf64eed61d
Ilya Tribusean <slash3b@gmail.com>
2024-04-05 19:39:14 +0300

    goget function to fetch and install go version

--- sources/config.fish
+++ sources/config.fish
@@ -67,3 +67,48 @@ end
 
 
 
+##### FUNCTIONS
+
+function godoc
+    go doc -all $argv | cat -n
+end
+
+function goget
+    set_color green; echo "requested $argv"; set_color normal
+
+    set -l dlfile "/home/slash3b/Downloads/$argv.linux-amd64.tar.gz"
+
+    if test ! -e $dlfile
+        set_color green; echo "downloading https://go.dev/dl/$argv.linux-amd64.tar.gz"; set_color normal
+
+        wget --output-document "/home/slash3b/Downloads/$argv.linux-amd64.tar.gz" "https://go.dev/dl/$argv.linux-amd64.tar.gz"
+
+        if test $status -ge 1
+            echo "failed to download https://go.dev/dl/$argv.linux-amd64.tar.gz"
+
+            return
+        end
+    else
+
+        echo "$argv tar.gz already downloaded"
+    end
+
+    echo "deleting existing go...."
+
+    rm -rf /home/slash3b/go
+
+    echo "unpacking $argv..."
+
+    tar -C /home/slash3b/ -xzf $dlfile
+    if test $status -ge 1
+        echo "failed to unpack $dlfile"
+
+        return
+    end
+
+    set_color green
+
+    echo "installed $argv!"
+
+end
+
