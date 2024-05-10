08d9ce3972f757dabd093889294645ce65063b8b
Ilya Tribusean <slash3b@gmail.com>
2024-05-10 22:42:05 +0300

    disable wakeup in a horrible way

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,8 @@
 #! /usr/bin/env bash
 
 sudo nixos-rebuild switch -I nixos-config=configuration.nix
+sudo echo "RP12" > /proc/acpi/wakeup
+sudo echo "GLAN" > /proc/acpi/wakeup
+sudo echo "XHC" > /proc/acpi/wakeup
+# https://gist.github.com/slash3b/c15e895e92ffec018b81fa40326088b7
+# hacky but for now is good
